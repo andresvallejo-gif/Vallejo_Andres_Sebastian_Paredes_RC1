c71963717f556d95a0027c4f6d9b93499fdfdc59
Sebastián Paredes y Andrés Vallejo <andres.vallejo.duque@udla.edu.ec>
2026-05-30 20:17:33 -0500

    Cálculo de promedios por asignatura y añadi el conteo de aprobados y reprobados en cada asignatura

--- hola.c
+++ hola.c
@@ -8,19 +8,30 @@ void ingresarNotas(float notas[][ASIGNATURAS], int estudiantes);
 void mostrarNotas(float notas[][ASIGNATURAS], int estudiantes);
 void calcularPromediosEstudiantes(float notas[][ASIGNATURAS], float promedios[], int estudiantes);
 void mostrarPromediosEstudiantes(float promedios[], int estudiantes);
+void calcularPromediosAsignaturas(float notas[][ASIGNATURAS], float promedios[], int estudiantes);
+void mostrarPromediosAsignaturas(float promedios[]);
+void contarAprobadosAsignaturas(float notas[][ASIGNATURAS], int aprobados[], int reprobados[], int estudiantes);
+void mostrarAprobadosAsignaturas(int aprobados[], int reprobados[]);
 
 int main(void) {
     int estudiantes;
     float notas[MAX_EST][ASIGNATURAS];
     float promediosEstudiantes[MAX_EST];
+    float promediosAsignaturas[ASIGNATURAS];
+    int aprobados[ASIGNATURAS];
+    int reprobados[ASIGNATURAS];
     void (*mostrar)(float (*)[ASIGNATURAS], int) = mostrarNotas;
 
     pedirEstudiantes(&estudiantes);
     ingresarNotas(notas, estudiantes);
     calcularPromediosEstudiantes(notas, promediosEstudiantes, estudiantes);
+    calcularPromediosAsignaturas(notas, promediosAsignaturas, estudiantes);
+    contarAprobadosAsignaturas(notas, aprobados, reprobados, estudiantes);
 
     mostrar(notas, estudiantes);
     mostrarPromediosEstudiantes(promediosEstudiantes, estudiantes);
+    mostrarPromediosAsignaturas(promediosAsignaturas);
+    mostrarAprobadosAsignaturas(aprobados, reprobados);
 
     return 0;
 }
@@ -70,5 +81,43 @@ void mostrarPromediosEstudiantes(float promedios[], int estudiantes) {
     printf("\nPromedio por estudiante:\n");
     for (int i = 0; i < estudiantes; i++) {
         printf("Estudiante %d: %.2f\n", i + 1, promedios[i]);
+    }
+}
+
+void calcularPromediosAsignaturas(float notas[][ASIGNATURAS], float promedios[], int estudiantes) {
+    for (int j = 0; j < ASIGNATURAS; j++) {
+        float suma = 0.0f;
+        for (int i = 0; i < estudiantes; i++) {
+            suma += notas[i][j];
+        }
+        promedios[j] = suma / estudiantes;
+    }
+}
+
+void mostrarPromediosAsignaturas(float promedios[]) {
+    printf("\nPromedio por asignatura:\n");
+    for (int j = 0; j < ASIGNATURAS; j++) {
+        printf("Asignatura %d: %.2f\n", j + 1, promedios[j]);
+    }
+}
+
+void contarAprobadosAsignaturas(float notas[][ASIGNATURAS], int aprobados[], int reprobados[], int estudiantes) {
+    for (int j = 0; j < ASIGNATURAS; j++) {
+        aprobados[j] = 0;
+        reprobados[j] = 0;
+        for (int i = 0; i < estudiantes; i++) {
+            if (notas[i][j] >= 6.0f) {
+                aprobados[j]++;
+            } else {
+                reprobados[j]++;
+            }
+        }
+    }
+}
+
+void mostrarAprobadosAsignaturas(int aprobados[], int reprobados[]) {
+    printf("\nAprobados y reprobados por asignatura:\n");
+    for (int j = 0; j < ASIGNATURAS; j++) {
+        printf("Asignatura %d: Aprobados %d, Reprobados %d\n", j + 1, aprobados[j], reprobados[j]);
     }
 }
